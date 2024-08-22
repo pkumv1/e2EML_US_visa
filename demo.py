7a71f230372fc79e26da3d182f03bfce836f6855
pkumv1 <pkumv1@gmail.com>
2024-08-22 18:52:40 +0530

    setup utils

---
+++ demo.py
@@ -0,0 +1,10 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+# logging.info("first custom log test")
+
+
+# try:
+#    2 / 0
+# except Exception as e:
+#    raise USvisaException(e, sys)
